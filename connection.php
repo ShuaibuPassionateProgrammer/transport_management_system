6fe292941029a2be19cd18648035155aa32044cf
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-05 21:57:19 +0100

    Initial commit

--- connection.php
+++ connection.php
@@ -1,4 +1,3 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '') or die ('Unable to connect. Check your connection parameters.');
 mysqli_select_db($db, 'tms_002' ) or die(mysqli_error($db));
-?>

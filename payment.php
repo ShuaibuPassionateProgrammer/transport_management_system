cbcde87e26523738da1966789c872fc5205c833a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-05 22:07:56 +0100

    Initial commit

--- payment.php
+++ payment.php
@@ -2,7 +2,17 @@
 if(isset($_GET['pid'])) {
     $pid = $_GET['pid'];
     ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
+</body>
+</html>
     <?php
 }
 else {

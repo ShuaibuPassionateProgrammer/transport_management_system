49288e610c6c8955692a07872079c76b97e0efce
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-05 22:23:52 +0100

    Initial commit

--- thank.php
+++ thank.php
@@ -11,6 +11,8 @@ if(isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TRANSPORT MANAGEMENT SYSTEM</title>
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     

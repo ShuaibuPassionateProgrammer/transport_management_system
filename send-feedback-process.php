10d5ddcab3cc3cd3e9fac08c794ff3ea2fbc21bb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-05 22:17:17 +0100

    Initial commit

--- send-feedback-process.php
+++ send-feedback-process.php
@@ -1,5 +1,11 @@
 <?php include('connection.php');?>
 
 <?php
-if(isset($_POST['send_feedback'])) {}
+if(isset($_POST['send_feedback'])) {
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+}
 ?>

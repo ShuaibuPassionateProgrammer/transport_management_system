f8234ecf182bd98ec546540e135e9505ef5c34a6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-07 21:46:04 +0100

    Initial commit

--- view-feedback-reply.php
+++ view-feedback-reply.php
@@ -12,7 +12,11 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- Page level plugin CSS-->
     <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-    <style></style>
+    <style>
+        html, body {
+            font-family: 'Tahoma', sans-serif;
+        }
+    </style>
 </head>
 <body>
     

6203ff874ed22be0159d5a8ee4ffd014737c8b06
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-07 21:38:35 +0100

    Initial commit

--- vehicle-booking-process.php
+++ vehicle-booking-process.php
@@ -3,7 +3,18 @@
 
 <?php
 if(isset($_POST['book_vehicle'])) {
-    
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $bus_name = $_POST['bus_name'];
+    $bus_type = $_POST['bus_type'];
+    $route_start = $_POST['route_start'];
+    $route_finish = $_POST['route_finish'];
+    $fair = $_POST['fair'];
+    $arrival = $_POST['arrival'];
+    $departure = $_POST['departure'];
+    $location = $_POST['location'];
+
 }
 else {
     header('location: vehicle-booking.php');

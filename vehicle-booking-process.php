224db75af006c988837d44e6a6e655943baee8a0
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-07 21:40:38 +0100

    Initial commit

--- vehicle-booking-process.php
+++ vehicle-booking-process.php
@@ -18,6 +18,9 @@ if(isset($_POST['book_vehicle'])) {
     if(empty($_POST['full_name']) || empty($_POST['phone']) || empty($_POST['bus_name']) || empty($_POST['bus_type']) /*|| empty($_POST['route_start']) || empty($_POST['route_finish'])*/ || empty($_POST['fair']) || empty($_POST['arrival']) || empty($_POST['departure']) || empty($_POST['location'])) {
         echo "<span style='background-color: #dc3545; color: #fafafa; padding: 2%; margin: 0 auto; width: 100%;'>All fields should not be blank/empty!</span>";
     }
+    else {
+        
+    }
 }
 else {
     header('location: vehicle-booking.php');

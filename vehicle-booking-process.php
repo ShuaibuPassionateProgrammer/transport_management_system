6ca5162b6946ed1c6eb5886b95b00a53bce9ea77
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-07 21:39:21 +0100

    Initial commit

--- vehicle-booking-process.php
+++ vehicle-booking-process.php
@@ -15,7 +15,8 @@ if(isset($_POST['book_vehicle'])) {
     $departure = $_POST['departure'];
     $location = $_POST['location'];
 
-    
+    if(empty($_POST['full_name']) || empty($_POST['phone']) || empty($_POST['bus_name']) || empty($_POST['bus_type']) /*|| empty($_POST['route_start']) || empty($_POST['route_finish'])*/ || empty($_POST['fair']) || empty($_POST['arrival']) || empty($_POST['departure']) || empty($_POST['location'])) {
+    }
 }
 else {
     header('location: vehicle-booking.php');

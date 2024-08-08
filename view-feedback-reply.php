a3c35630629d04258b0e8c61d38c84b4333bd32a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-08 21:47:03 +0100

    Initial commit

--- view-feedback-reply.php
+++ view-feedback-reply.php
@@ -61,5 +61,8 @@
             <a href="../transport-management-system/">Back to Homepage</a>
         </p>
     </div>
+
+    <script src="./vendor/jquery/jquery.min.js"></script>
+    <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>

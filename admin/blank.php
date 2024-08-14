d29c1739661b45957930d93147daa477803b7eef
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-14 22:22:23 +0100

    Working on Admin

--- admin/blank.php
+++ admin/blank.php
@@ -19,5 +19,31 @@
 <body id="page-top">
     <?php include'header.php'; ?>
     
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.php">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Blank Page</li>
+          </ol>
+
+          <!-- Page Content -->
+          <h1>Blank Page</h1>
+          <hr>
+          <p>This is a great starting point for new custom pages.</p>
+
+        </div>
+        <!-- /.container-fluid -->
+
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
 </body>
 </html>

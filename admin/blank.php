bfbf1c5d152a412b9a9895a8468232bf2e0d8e2c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-14 22:22:34 +0100

    Working on Admin

--- admin/blank.php
+++ admin/blank.php
@@ -21,7 +21,6 @@
     
     <div id="content-wrapper">
         <div class="container-fluid">
-
           <!-- Breadcrumbs-->
           <ol class="breadcrumb">
             <li class="breadcrumb-item">

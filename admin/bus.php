1ffddbe7b3ef61596bde618f0e2c726c976229eb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-15 22:26:35 +0100

    Working on Admin

--- admin/bus.php
+++ admin/bus.php
@@ -5,7 +5,7 @@ include 'topnav.php';
 
 <div class="col-lg-12">
     <div>
-            <i class="fas fa-table"></i>
+        <i class="fas fa-table"></i>
 
             Bus Records  <a href="busadd.php?action=add" type="button" class="btn btn-xs btn-primary">Add New</a>
             </div>    

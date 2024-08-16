f314d24e70a44695e374b0cb737d7a8a9a588891
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-16 10:27:42 +0100

    Working on Admin

--- admin/bus.php
+++ admin/bus.php
@@ -12,7 +12,7 @@ include 'topnav.php';
     <br> </br>
     
     <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                 <thead>
                                     <tr>
                                         <th>Bus Name</th>

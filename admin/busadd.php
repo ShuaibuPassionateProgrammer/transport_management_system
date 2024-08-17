a87f46b923af2583ad9aa7eaa324d9125c7a4151
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-17 22:32:52 +0100

    Working on Admin

--- admin/busadd.php
+++ admin/busadd.php
@@ -5,7 +5,7 @@ include 'topnav.php';
 
 <div class="contanier"> 
     <div class="card card-register mx-auto mt-5">
-<div class="card-header"><h2>Add new Vehicle</h2> </div>
+        <div class="card-header"><h2>Add new Vehicle</h2> </div>
 <div class="card-body">
                  
 

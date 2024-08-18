afa72eb101e76025353cecc19c83bacc7534094a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-18 10:34:49 +0100

    Working on Admin

--- admin/busdel.php
+++ admin/busdel.php
@@ -1,11 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<?php
+       
+       include('connection.php');
+       include('header.php');
+       
+        ?>  
+
 <body>
-    
+<?php
+
+	
+
+			if (!isset($_GET['do']) || $_GET['do'] != 1) {
+								
+	
+			switch ($_GET['type']) {
+				case 'bus':
+					$query = 'DELETE FROM bus
+							WHERE
+							BUS_ID = ' . $_GET['id'];
+						$result = mysqli_query($db, $query) or die(mysqli_error($db));
+						
+?>
+			<script type="text/javascript">
+				alert("Successfully Deleted.");
+				window.location = " bus.php";
+			</script>				
+				
+			<?php
+			//break;
+				}
+			}
+			?>
+
 </body>
 </html>

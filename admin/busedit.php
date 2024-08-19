08747c6d938ef4ceacabeb39b2ee50b4e6b34431
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-19 22:50:30 +0100

    Working on Admin

--- admin/busedit.php
+++ admin/busedit.php
@@ -29,7 +29,7 @@ include 'topnav.php';
                             <input type="hidden" name="id" value="<?php echo $zz; ?>" />
                         </div>
                         <div class="form-group">
-                        <input class="form-control" placeholder="Bus Name" name="BUS_NAME" value="<?php echo $i; ?>">
+                            <input class="form-control" placeholder="Bus Name" name="BUS_NAME" value="<?php echo $i; ?>">
                         </div>
                         <div class="form-group">
                         <input class="form-control" placeholder="Bus Type" name="BUS_TYPE" value="<?php echo $a; ?>">

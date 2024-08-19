3cf7503dde37c1709010c242648e71db8a5c2b2c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-19 22:39:31 +0100

    Working on Admin

--- admin/busedit.php
+++ admin/busedit.php
@@ -7,7 +7,7 @@ include 'topnav.php';
 <div class="card card-register mx-auto mt-5">
 <?php 
 $query = 'SELECT * FROM bus WHERE BUS_ID ='.$_GET['id'];
-            $result = mysqli_query($db, $query) or die(mysqli_error($db));
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
               while($row = mysqli_fetch_array($result))
               {   
                 $zz= $row['BUS_ID'];

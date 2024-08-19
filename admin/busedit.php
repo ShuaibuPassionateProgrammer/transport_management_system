de66aea1b4a2c654ea0bc9b61dfcd635e76aed93
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-19 22:51:31 +0100

    Working on Admin

--- admin/busedit.php
+++ admin/busedit.php
@@ -23,25 +23,25 @@ include 'topnav.php';
 
         <div class="col-lg-12" style="box-shadow: rgba(2,2,2,0,2);">
             <h1>Edit Records</h1>
-                <div class="col-lg-6"> 
-                    <form role="form" method="post" action="busedit1.php">
-                        <div class="form-group">
-                            <input type="hidden" name="id" value="<?php echo $zz; ?>" />
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Bus Name" name="BUS_NAME" value="<?php echo $i; ?>">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Bus Type" name="BUS_TYPE" value="<?php echo $a; ?>">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="DRIVER ID" name="DRIVER_ID" value="<?php echo $b; ?>">
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Update Record</button>
-                        </div>
-                </form>  
-                </div>
+            <div class="col-lg-6"> 
+                <form role="form" method="post" action="busedit1.php">
+                    <div class="form-group">
+                        <input type="hidden" name="id" value="<?php echo $zz; ?>" />
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Bus Name" name="BUS_NAME" value="<?php echo $i; ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Bus Type" name="BUS_TYPE" value="<?php echo $a; ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="DRIVER ID" name="DRIVER_ID" value="<?php echo $b; ?>">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Update Record</button>
+                    </div>
+            </form>  
+            </div>
             </div>
             
         </div>

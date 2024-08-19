bc7a7b2adf19372f146fd3b577f2228ec4e60fda
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-19 22:39:41 +0100

    Working on Admin

--- admin/busedit.php
+++ admin/busedit.php
@@ -8,7 +8,8 @@ include 'topnav.php';
 <?php 
 $query = 'SELECT * FROM bus WHERE BUS_ID ='.$_GET['id'];
 $result = mysqli_query($db, $query) or die(mysqli_error($db));
-              while($row = mysqli_fetch_array($result))
+
+while($row = mysqli_fetch_array($result))
               {   
                 $zz= $row['BUS_ID'];
                 $i= $row['BUS_NAME'];

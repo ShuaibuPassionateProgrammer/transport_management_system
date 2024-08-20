1ec84605433cf5d3637bfaa11e280b971f5f6df7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-20 10:55:46 +0100

    Working on Admin

--- admin/busedit1.php
+++ admin/busedit1.php
@@ -10,11 +10,8 @@ $bname = $_POST['BUS_NAME'];
 $btype = $_POST['BUS_TYPE'];
 $did= $_POST['DRIVER_ID'];
 			
-		
-	 			$query = 'UPDATE bus set BUS_NAME ="'.$bname.'",
-					BUS_TYPE ="'.$btype.'", DRIVER_ID="'.$did.'" WHERE
-					BUS_ID ="'.$zz.'"';
-					$result = mysqli_query($db, $query) or die(mysqli_error($db));
+$query = 'UPDATE bus set BUS_NAME ="'.$bname.'", BUS_TYPE ="'.$btype.'", DRIVER_ID="'.$did.'" WHERE BUS_ID ="'.$zz.'"';
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
 							
 ?>	
 	<script type="text/javascript">

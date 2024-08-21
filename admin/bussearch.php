200b7d2dfed4b62fdae6cb7bee9d17fc613a3518
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-21 22:59:45 +0100

    Working on Admin

--- admin/bussearch.php
+++ admin/bussearch.php
@@ -13,14 +13,14 @@ include 'topnav.php'
             $result = mysqli_query($db, $query) or die(mysqli_error($db));
             
             while($row = mysqli_fetch_array($result))
-                        {   
-                            $zz= $row['BUS_ID'];
-                            $i= $row['BUS_NAME'];
-                            $a=$row['BUS_TYPE'];
-                            $b=$row['DRIVER_ID'];
-                        }
-                        
-                        $id = $_GET['id'];
+            {   
+                $zz= $row['BUS_ID'];
+                $i= $row['BUS_NAME'];
+                $a=$row['BUS_TYPE'];
+                $b=$row['DRIVER_ID'];
+            }
+            
+            $id = $_GET['id'];
                     
             ?>
 

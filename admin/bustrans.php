f7ffd90e610a650f80bcc745cd1bc3def7d8531f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-22 11:06:36 +0100

    Working on Admin

--- admin/bustrans.php
+++ admin/bustrans.php
@@ -7,18 +7,17 @@
     $bname= $_POST['BUS_NAME'];
     $btype= $_POST['BUS_TYPE'];
     $did= $_POST['DRIVER_ID'];
+
+    switch($_GET['action']){
+        case 'add':			
+                $query = "INSERT INTO bus
+                (BUS_NAME, BUS_TYPE, DRIVER_ID)
+                VALUES ('".$bname."','".$btype."','".$did."')";
+                mysqli_query($db,$query)or die (mysqli_error($db));
             
-    
-        switch($_GET['action']){
-            case 'add':			
-                    $query = "INSERT INTO bus
-                    (BUS_NAME, BUS_TYPE, DRIVER_ID)
-                    VALUES ('".$bname."','".$btype."','".$did."')";
-                    mysqli_query($db,$query)or die (mysqli_error($db));
-                
-            break;
-                        
-            }
+        break;
+                    
+    }
     ?>
     	<script type="text/javascript">
 			alert("Successfully added.");

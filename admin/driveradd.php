8440f5cca7bb87c3023eec5f389a359cf3845fdb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-25 23:16:27 +0100

    Working on Admin

--- admin/driveradd.php
+++ admin/driveradd.php
@@ -26,19 +26,19 @@ include 'topnav.php';
                 $query = 'SELECT * FROM driver';
                 $result = mysqli_query($db, $query) or die (mysqli_error($db));
 
-    while ($row = mysqli_fetch_assoc($result)) {
-                            
-        echo '<tr>';
-        echo '<td>'. $row['DRIVER_NAME'].'</td>';
-        echo '<td>'. $row['DRIVER_EMAIL'].'</td>';
-        echo '<td>'. $row['DRIVER_PHONE'].'</td>';
-        echo '<td>'. $row['EMPLOY_DATE'].'</td>';
-        echo '<td> <a type="button" class="btn btn-xs btn-primary" href="driversearch.php?action=edit & id='.$row['DRIVER_ID'] . '" > SEARCH </a> ';
-        echo ' <a  type="button" class="btn btn-xs btn-warning" href="driveredit.php?action=edit & id='.$row['DRIVER_ID'] . '"> EDIT </a> ';
-        echo ' <a  type="button" class="btn btn-xs btn-danger" href="driverdel.php?type=driver&delete & id='.$row['DRIVER_ID'] . '" onclick="return confirm(\'Are you sure, you want to Delete a Driver\');">DELETE </a> </td>';
-        echo '</tr> ';
-}
-?> 
+                while ($row = mysqli_fetch_assoc($result)) {
+                                        
+                    echo '<tr>';
+                    echo '<td>'. $row['DRIVER_NAME'].'</td>';
+                    echo '<td>'. $row['DRIVER_EMAIL'].'</td>';
+                    echo '<td>'. $row['DRIVER_PHONE'].'</td>';
+                    echo '<td>'. $row['EMPLOY_DATE'].'</td>';
+                    echo '<td> <a type="button" class="btn btn-xs btn-primary" href="driversearch.php?action=edit & id='.$row['DRIVER_ID'] . '" > SEARCH </a> ';
+                    echo ' <a  type="button" class="btn btn-xs btn-warning" href="driveredit.php?action=edit & id='.$row['DRIVER_ID'] . '"> EDIT </a> ';
+                    echo ' <a  type="button" class="btn btn-xs btn-danger" href="driverdel.php?type=driver&delete & id='.$row['DRIVER_ID'] . '" onclick="return confirm(\'Are you sure, you want to Delete a Driver\');">DELETE </a> </td>';
+                    echo '</tr> ';
+                }
+                ?> 
                 
             </tbody>
         </table>

dd8f3de732dbd62c7ff1406b6cf259c42763fa61
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-26 11:18:50 +0100

    Working on Admin

--- admin/driverdel.php
+++ admin/driverdel.php
@@ -12,8 +12,7 @@ $edate = $_POST['EMPLOY_DATE'];
 
 
 $query = 'UPDATE driver set DRIVER_NAME ="'.$dname.'", DRIVER_EMAIL ="'.$demail.'", DRIVER_PHONE ="'.$dphone.'", EMPLOY_DATE ="'.$edate.'" WHERE DRIVER_ID ="'.$d_id.'"';
-$result = mysqli_query($db, $query) or die(mysqli_error($db));
-                
+$result = mysqli_query($db, $query) or die(mysqli_error($db));                
 ?>	
 <script type="text/javascript">
 alert("Update Successfull.");

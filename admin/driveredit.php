eda1ae1eaf52b404404d0cd251312f5e41fb2bba
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-26 11:21:27 +0100

    Working on Admin

--- admin/driveredit.php
+++ admin/driveredit.php
@@ -6,9 +6,7 @@ include 'topnav.php';
 <div class="contanier">
     <div class="card card-register mx-auto mt-5">
     <?php 
-    $query = 'SELECT * FROM driver
-              WHERE
-              DRIVER_ID ='.$_GET['id'];
+    $query = 'SELECT * FROM driver WHERE DRIVER_ID ='.$_GET['id'];
             $result = mysqli_query($db, $query) or die(mysqli_error($db));
               while($row = mysqli_fetch_array($result))
               {   

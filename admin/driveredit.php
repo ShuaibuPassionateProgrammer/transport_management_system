e2de7bee9a0013e3c4dc75b96b4d56781713caa2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-26 11:22:39 +0100

    Working on Admin

--- admin/driveredit.php
+++ admin/driveredit.php
@@ -21,34 +21,34 @@ include 'topnav.php';
     $id = $_GET['id'];
     ?>
 
-             <div class="col-lg-12 p-4">
-                      <h2 class="ml-4">Edit Driver Records</h2>
-                      <!-- <div class="col-lg-6"> -->
+    <div class="col-lg-12 p-4">
+            <h2 class="ml-4">Edit Driver Records</h2>
+            <!-- <div class="col-lg-6"> -->
 
-                      <form role="form" method="post" action="driveredit1.php">
-                            <div class="form-group">
-                                <input type="hidden" name="id" value="<?php echo $did; ?>" />
-                            </div>
-                            <div class="card-body">
-                              <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Driver Name" name="DRIVER_NAME" value="<?php echo $dname; ?>" required>
-                              </div>
-                              <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Driver Email" name="DRIVER_EMAIL" value="<?php echo $demail; ?>" required>
-                              </div>
-                              <div class="form-group">
-                                <input type="tel" class="form-control" placeholder="Driver Phone" name="DRIVER_PHONE" value="<?php echo $dphone; ?>" required>
-                              </div>
-                              <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Employ Date" name="EMPLOY_DATE" value="<?php echo $edate; ?>" required>
-                              </div>
-                              <button type="submit" class="btn btn-primary">Update Driver</button>
-                      </form> 
-                    <!-- </div> -->
+            <form role="form" method="post" action="driveredit1.php">
+                <div class="form-group">
+                    <input type="hidden" name="id" value="<?php echo $did; ?>" />
                 </div>
-                
-            </div>
-            <!-- /.container-fluid -->
+                <div class="card-body">
+                    <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Driver Name" name="DRIVER_NAME" value="<?php echo $dname; ?>" required>
+                    </div>
+                    <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Driver Email" name="DRIVER_EMAIL" value="<?php echo $demail; ?>" required>
+                    </div>
+                    <div class="form-group">
+                    <input type="tel" class="form-control" placeholder="Driver Phone" name="DRIVER_PHONE" value="<?php echo $dphone; ?>" required>
+                    </div>
+                    <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Employ Date" name="EMPLOY_DATE" value="<?php echo $edate; ?>" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update Driver</button>
+            </form> 
+        <!-- </div> -->
+    </div>
+    
+</div>
+<!-- /.container-fluid -->
 
         </div>
         <?php include 'footer.php'; ?>

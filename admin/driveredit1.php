2e75acabd573513a7cad7c24ebad954fabfb36c7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-27 20:43:29 +0100

    Working on Admin

--- admin/driveredit1.php
+++ admin/driveredit1.php
@@ -12,7 +12,7 @@ $query = 'UPDATE driver set DRIVER_NAME ="'.$dname.'", DRIVER_EMAIL ="'.$demail.
 $result = mysqli_query($db, $query) or die(mysqli_error($db));
 ?>	
 <script type="text/javascript">
-			alert("Update Successfull.");
+	alert("Update Successfull.");
 			window.location = "driver.php";
 </script>
  <?php include 'footer.php'; ?>

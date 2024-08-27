136787aea1d54e5489477e587313ae1b61916765
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-27 20:42:14 +0100

    Working on Admin

--- admin/driveredit1.php
+++ admin/driveredit1.php
@@ -7,8 +7,7 @@ $dname = $_POST['DRIVER_NAME'];
 $demail = $_POST['DRIVER_EMAIL'];
 $dphone = $_POST['DRIVER_PHONE'];
 $edate = $_POST['EMPLOY_DATE'];
-			
-		
+					
 $query = 'UPDATE driver set DRIVER_NAME ="'.$dname.'",
     DRIVER_EMAIL ="'.$demail.'",
     DRIVER_PHONE ="'.$dphone.'",

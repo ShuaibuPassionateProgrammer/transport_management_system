1cd13c3f0c73ecf2bc98bac967351607f0040a8d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-27 20:42:21 +0100

    Working on Admin

--- admin/driveredit1.php
+++ admin/driveredit1.php
@@ -13,8 +13,7 @@ $query = 'UPDATE driver set DRIVER_NAME ="'.$dname.'",
     DRIVER_PHONE ="'.$dphone.'",
     EMPLOY_DATE ="'.$edate.'" WHERE
     DRIVER_ID ="'.$d_id.'"';
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
-							
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));				
 ?>	
 	<script type="text/javascript">
 			alert("Update Successfull.");

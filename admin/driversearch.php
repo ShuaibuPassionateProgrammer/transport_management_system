7b19c0d6d3dfc898a41c89a9d0bd6689e714820a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-28 08:39:37 +0100

    Working on Admin

--- admin/driversearch.php
+++ admin/driversearch.php
@@ -8,8 +8,7 @@ include 'topnav.php'
         <div class="card-header"> <h2>Check Record</h2> </div>
         <div class="card-body">
         <?php
-        $query = 'SELECT * FROM driver WHERE
-                    DRIVER_ID ='.$_GET['id'];
+        $query = 'SELECT * FROM driver WHERE DRIVER_ID ='.$_GET['id'];
                     $result = mysqli_query($db, $query) or die(mysqli_error($db));
                     while($row = mysqli_fetch_array($result))
                     {   

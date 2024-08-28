6dabcac75fb05dc5a7ffff3321f2a5384a7a18f5
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-28 08:41:20 +0100

    Working on Admin

--- admin/driversearch.php
+++ admin/driversearch.php
@@ -5,7 +5,8 @@ include 'topnav.php'
 
 <div class="contanier">
     <div class="card card-register mx-auto mt-5">
-        <div class="card-header"> <h2>Check Record</h2> </div>
+        <div class="card-header">
+            <h2>Check Record</h2> </div>
         <div class="card-body">
             <?php
             $query = 'SELECT * FROM driver WHERE DRIVER_ID ='.$_GET['id'];

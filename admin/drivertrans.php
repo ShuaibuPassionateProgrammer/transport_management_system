1453f76ff42bc7e1b03e1682e33d239e41dfaf10
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-29 08:47:15 +0100

    Working on Admin

--- admin/drivertrans.php
+++ admin/drivertrans.php
@@ -1,14 +1,12 @@
-<?php include'header.php' ;?>
-<?php include'connection.php' ;?>
+<?php include'header.php'; ?>
+<?php include'connection.php'; ?>
 
 <div class="col-lg-12">
     <?php
     $dname= $_POST['DRIVER_NAME'];
     $demail= $_POST['DRIVER_EMAIL'];
     $dphone= $_POST['DRIVER_PHONE'];
-            
-            
-    
+
     switch($_GET['action']){
         case 'add':			
             $query = "INSERT INTO driver (DRIVER_NAME,DRIVER_EMAIL,DRIVER_PHONE,EMPLOY_DATE) VALUES ('".$dname."','".$demail."','".$dphone."',NOW())";

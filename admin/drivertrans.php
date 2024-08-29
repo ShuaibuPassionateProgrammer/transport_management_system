42dc3e39436384adf27894e22a9cc7802a9bd802
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-29 08:45:32 +0100

    Working on Admin

--- admin/drivertrans.php
+++ admin/drivertrans.php
@@ -2,24 +2,24 @@
 <?php include'connection.php' ;?>
 
 <div class="col-lg-12">
-                <?php
-						$dname= $_POST['DRIVER_NAME'];
-                		$demail= $_POST['DRIVER_EMAIL'];
-					    $dphone= $_POST['DRIVER_PHONE'];
-						
-						
-				
-					switch($_GET['action']){
-						case 'add':			
-								$query = "INSERT INTO driver
-								(DRIVER_NAME,DRIVER_EMAIL,DRIVER_PHONE,EMPLOY_DATE)
-								VALUES ('".$dname."','".$demail."','".$dphone."',NOW())";
-								mysqli_query($db,$query)or die (mysqli_error($db));
-							
-						break;
-									
-						}
-				?>
+    <?php
+            $dname= $_POST['DRIVER_NAME'];
+            $demail= $_POST['DRIVER_EMAIL'];
+            $dphone= $_POST['DRIVER_PHONE'];
+            
+            
+    
+        switch($_GET['action']){
+            case 'add':			
+                    $query = "INSERT INTO driver
+                    (DRIVER_NAME,DRIVER_EMAIL,DRIVER_PHONE,EMPLOY_DATE)
+                    VALUES ('".$dname."','".$demail."','".$dphone."',NOW())";
+                    mysqli_query($db,$query)or die (mysqli_error($db));
+                
+            break;
+                        
+            }
+    ?>
     	<script type="text/javascript">
 			alert("Successfully added.");
 			window.location = "driver.php";

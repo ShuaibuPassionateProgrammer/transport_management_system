af915553a046240e3afbbf9cdece6f4d19f12237
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-30 08:52:41 +0100

    Working on Admin

--- admin/feedback.php
+++ admin/feedback.php
@@ -24,7 +24,30 @@
         $query = mysqli_query($db, $sql);
 
         if(mysqli_num_rows($query) > 0) {
-            while ($row=mysqli_fetch_assoc($query)) {}
+            while ($row=mysqli_fetch_assoc($query)) {
+                <tr>
+                    <td><?=$row['id']?></td>
+                    <td><?=$row['fullname']?></td>
+                    <td><?=$row['email']?></td>
+                    <td><?=$row['phone']?></td>
+                    <td><?=$row['subject']?></td>
+                    <td><?=$row['message']?></td>
+                    <td class="text-center">
+                        <a href="feedbackedit.php?fid=<?php echo $row['id']?>" class="btn btn-info">Edit</a>
+                        <a href="feedbackreply.php?freply=<?php echo $row['id']?>" class="btn btn-success">Reply</a>
+                        <a href="feedbackdel.php?fdel=<?php echo $row['id']?>" class="btn btn-danger" onclick="return confirm('Are you sure, you want to Delete a FeedBack');">Delete</a>
+                    </td>
+                </tr>
+                <?php
+            }
+        }
+        else {
+            ?>
+            <td colspan="7"><p class="text-danger">FeedBack not available!</p></td>
+            <?php
+        }
+        ?>
+            }
         }
         ?>
     </tbody>

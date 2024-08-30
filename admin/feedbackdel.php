987200fa5f60572bed5bdab64e5e34d5cb598058
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-30 08:56:39 +0100

    Working on Admin

--- admin/feedbackdel.php
+++ admin/feedbackdel.php
@@ -6,7 +6,9 @@ if(isset($_GET['?fdel'])) {
     $sql = "DELETE FROM feedback WHERE id = $fid";
     $query = mysqli_query($db, $sql);
 
-    if($query) {}
+    if($query) {
+        echo "<script>alert('FeedBack Deleted Successfully!');window.location.href='feedback.php'</script>";
+    }
 }
 else {
     header('location: feedback.php');

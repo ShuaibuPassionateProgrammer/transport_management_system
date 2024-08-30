1041ff35e007be7e3e632c7fb06378c75f79ed2f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-08-30 09:02:03 +0100

    Working on Admin

--- admin/feedbackedit1.php
+++ admin/feedbackedit1.php
@@ -1,5 +1,12 @@
 <?php include('connection.php')?>
 
 <?php
-if(isset($_POST['update_feedback'])) {}
+if(isset($_POST['update_feedback'])) {
+    $id = $_POST['id'];
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+}
 ?>

c5336f145906ea5231698ee4a25d65bee48df603
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-01 09:10:57 +0100

    Working on Admin

--- admin/feedbackreply1.php
+++ admin/feedbackreply1.php
@@ -3,5 +3,7 @@
 if(isset($_POST['reply_feedback'])) {
     $id = $_POST['id'];
     $message = $_POST['message'];
+
+    $sql = "UPDATE feedback SET replymessage = '$message' WHERE id = $id";
 }
 ?>

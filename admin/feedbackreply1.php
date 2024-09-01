03485ed61bb6ae8f4542bc05c0a2093d50cd56c9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-01 09:12:42 +0100

    Working on Admin

---
+++ admin/feedbackreply1.php
@@ -0,0 +1,20 @@
+<?php include('connection.php')?>
+<?php
+if(isset($_POST['reply_feedback'])) {
+    $id = $_POST['id'];
+    $message = $_POST['message'];
+
+    $sql = "UPDATE feedback SET replymessage = '$message' WHERE id = $id";
+    $query = mysqli_query($db, $sql);
+
+    if($query) {
+        echo "<script>alert('FeedBack Replied Successfully!');window.location.href='feedback.php'</script>";
+    }
+    else {
+        echo "<script>alert('FeedBack Reply Failed!');window.location.href='feedback.php'</script>";
+    }
+}
+else {
+    header('location: feedback.php');
+}
+?>

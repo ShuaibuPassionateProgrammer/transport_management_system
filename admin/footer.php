de429670a29cf3e484f606c4c6a88b879c9155e7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-02 09:15:48 +0100

    Working on Admin

--- admin/footer.php
+++ admin/footer.php
@@ -46,3 +46,7 @@
 
     <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>

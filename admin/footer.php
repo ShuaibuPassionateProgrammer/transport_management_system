6810037fbdae3fdbca22031eb6e3b376a36fd402
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-02 09:15:07 +0100

    Working on Admin

--- admin/footer.php
+++ admin/footer.php
@@ -15,3 +15,8 @@
 
       </div>
     <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top" style="background-image: linear-gradient(black, #014923);>
+      <i class="fas fa-angle-up"></i>
+    </a>

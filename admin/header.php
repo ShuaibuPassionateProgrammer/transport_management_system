91575a6fdddb078acceeb233ec72542dd9ab8c34
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-03 08:29:12 +0100

    Working on Admin

--- admin/header.php
+++ admin/header.php
@@ -17,6 +17,147 @@
     <title>TRANSPORT MANAGEMENT SYSTEM</title>
 </head>
 <body id="page-top">
+
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1" href="index.php"> BUS SCHEDULE </a>
+
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Navbar Search -->
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+        
+      </form>
+
+      <!-- Navbar -->
+      <ul class="navbar-nav ml-auto ml-md-0">
+        <!--<li class="nav-item dropdown no-arrow mx-1">
+          <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bell fa-fw"></i>
+            <span class="badge badge-danger">9+</span>
+          </a>
+         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown no-arrow mx-1">
+          <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-envelope fa-fw"></i>
+            <span class="badge badge-danger">7</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>-->
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle fa-fw"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="register.php">Register</a>
+            <a class="dropdown-item" href="signin.php">Sign in</a>
+            <a class="dropdown-item" href="login.php" data-toggle="modal" data-target="#logoutModal">Logout</a>
+            
+          </div>
+        </li>
+      </ul>
+
+    </nav>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-home"></i>
+            <span>HOMEPAGE</span>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw  fa-cloud"></i>
+            <span>SERVICES</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item " href="urns.php">URNS</a> 
+            <a class="dropdown-item " href="caskets.php">CASKETS</a>
+            <a class="dropdown-item " href="vehicles.php">VEHICLES</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="clients.php">
+            <i class="fas fa-fw fa-bus  "></i>
+            <span>Bus</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="corpses.php">
+            <i class="fas fa-fw fa-users    "></i>
+            <span>Driver</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="route.php">
+            <i class="fas fa-fw fa-route  "></i>
+            <span>Route</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="department.php">
+            <i class="fas fa-fw fa-clock   "></i>
+            <span>Schedule</span></a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="stop.php">
+            <i class="fas fa fa-exclamation-triangle "></i>
+            <span>Stop</span></a>
+          </li>
+      </ul>
+
+      <div id="content-wrapper">
+
+        
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+        <footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website <?php echo date('Y');?></span>
+            </div>
+          </div>
+        </footer>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="logout.php">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
     
 </body>
 </html>

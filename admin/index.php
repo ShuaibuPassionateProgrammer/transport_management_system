527c8bcdfa36b9421ffc836f25f1f44398d93833
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-04 20:36:29 +0100

    Working on Admin

--- admin/index.php
+++ admin/index.php
@@ -3,21 +3,21 @@ include 'connection.php';
 include 'topnav.php';
 ?>
 
-          <!-- Breadcrumbs-->
-          <ol class="breadcrumb"
-            <li class="breadcrumb-item" style="background-image: linear-gradient();">
-              <a href="index."> <h2> TRANSPORT MANAGEMENT SYSTEM </h2> </a>
-            </li>
-           
-          </ol>
+<!-- Breadcrumbs-->
+<ol class="breadcrumb"
+<li class="breadcrumb-item" style="background-image: linear-gradient();">
+    <a href="index."> <h2> TRANSPORT MANAGEMENT SYSTEM </h2> </a>
+</li>
 
-          <div class="bus_logo">
-            <a class="image full"><img src="image/deluxe.jpg" style="width:800px;" style="background-attachment: fixed;" style="background-size: contain;"  > </a>
+</ol>
 
-         <!-- Page Content -->
-          <h4><h4/>
-          
-          <hr>
-          <p> <h5> </h5></p>
+<div class="bus_logo">
+<a class="image full"><img src="image/deluxe.jpg" style="width:800px;" style="background-attachment: fixed;" style="background-size: contain;"  > </a>
+
+<!-- Page Content -->
+<h4><h4/>
+
+<hr>
+<p> <h5> </h5></p>
 
 <?php include 'footer.php'; ?>

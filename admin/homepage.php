95a964de8db788b59dcfe780c3496c93674bd1f9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-04 20:32:23 +0100

    Working on Admin

--- admin/homepage.php
+++ admin/homepage.php
@@ -9,7 +9,6 @@ confirm_logged_in();
 <!DOCTYPE html>
 <html lang="en">
   <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">

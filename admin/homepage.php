befdc6e43f85577787b2d3eab4aae49a9a066412
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-04 20:33:58 +0100

    Working on Admin

--- admin/homepage.php
+++ admin/homepage.php
@@ -32,9 +32,7 @@ confirm_logged_in();
       </button>
 
       <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        
-      </form>
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0"></form>
 
       <!-- Navbar -->
       <ul class="navbar-nav ml-auto ml-md-0">

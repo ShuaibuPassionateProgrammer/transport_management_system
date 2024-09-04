0552b14c5f7bb4f461399f85de24777fa4fedfae
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-04 20:33:03 +0100

    Working on Admin

--- admin/homepage.php
+++ admin/homepage.php
@@ -21,7 +21,6 @@ confirm_logged_in();
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- Page level plugin CSS-->
     <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-
     <!-- Custom styles for this template-->
     <link href="css/sb-admin.css" rel="stylesheet">
 

10b2e067637ba6a41bfe377c224d46c6df45e8b5
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-04 20:32:30 +0100

    Working on Admin

--- admin/homepage.php
+++ admin/homepage.php
@@ -14,7 +14,6 @@ confirm_logged_in();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>BUS SCHEDULING SYSTEM</title>
 
     <!-- Bootstrap core CSS-->

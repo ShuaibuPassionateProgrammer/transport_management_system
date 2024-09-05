ab4ddfa36e86dd1e052eed13fbe58669ef3ab86f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-05 08:42:39 +0100

    Working on Admin

--- admin/login.php
+++ admin/login.php
@@ -12,6 +12,8 @@
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
 </head>
 <body>
     

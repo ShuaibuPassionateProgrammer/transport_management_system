7dc5b4ef9f0862bc86bad03aee1a384b57e02dd6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-05 08:44:18 +0100

    Working on Admin

--- admin/login.php
+++ admin/login.php
@@ -55,5 +55,9 @@
         </div>
       </div>
     </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>

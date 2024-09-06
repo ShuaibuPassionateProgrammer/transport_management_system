da7d4772ef9ee6025f89b7f13610009825f72d67
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-06 10:53:19 +0100

    Working on Admin

--- admin/route.php
+++ admin/route.php
@@ -2,8 +2,7 @@
 include 'connection.php';
 include 'topnav.php';?>
            
-<div class="col-lg-12">
-                        <div> 
+<div class="col-lg-12"><div> 
 <i class="fas fa-table"></i>
 
                Route Records  <a href="routeadd.php?action=add" type="button" class="btn btn-xs btn-primary">Add New</a>

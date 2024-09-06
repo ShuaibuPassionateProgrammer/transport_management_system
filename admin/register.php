0ea99ea280f2255698fe3088fec11e3f463f90a8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-06 10:51:31 +0100

    Working on Admin

--- admin/register.php
+++ admin/register.php
@@ -15,5 +15,57 @@
     <title>Admin Dashboard | Register</title>
 </head>
 <body class="bg-dark" style="background-image: linear-gradient(gray, #2144);">
+    <div class="container">
+      <div class="card card-register mx-auto mt-5">
+        <div class="card-header">Register an Account</div>
+        <div class="card-body">
+          <form method="POST" action="success1.php">
+            <div class="form-group">
+              <div class="form-row">
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <input type="text" id="firstName" class="form-control" placeholder="First name" name="firstname" required="required" autofocus="autofocus">
+                    <label for="firstName">First name</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <input type="text" id="lastName" class="form-control" placeholder="Last name" name="lastname" required="required">
+                    <label for="lastName">Last name</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required="required">
+                <label for="inputEmail">Email address</label>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="form-row">
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required="required">
+                    <label for="inputPassword">Password</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm password" name="confirm" required="required">
+                    <label for="confirmPassword">Confirm password</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block" name="register" style="background-image: linear-gradient(, #2144);">Register</button>
+          </form>
+          <div class="text-center">
+            <a class="d-block small mt-3" href="login.php" Login Page</a>
+            <a class="d-block small" href="forgot-password.php">Forgot Password?</a>
+          </div>
+        </div>
+      </div>
+    </div>
 </body>
 </html>

53d0a25b2c9904186f597b0eff06ab725b81776b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-06 10:50:34 +0100

    Working on Admin

--- admin/register.php
+++ admin/register.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Admin Dashboard | Register</title>
 </head>
 <body>

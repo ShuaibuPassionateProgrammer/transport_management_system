e48d9eeac6c554a73c369d2dc2319fa3378719c2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-08 18:27:34 +0100

    Done with Route Deletion

--- admin/routedel.php
+++ admin/routedel.php
@@ -4,31 +4,23 @@ include('header.php');
 ?>  
 
 <body>
-<?php
-
-	
-
-			if (!isset($_GET['do']) || $_GET['do'] != 1) {
-								
-	
-			switch ($_GET['type']) {
-				case 'route':
-					$query = 'DELETE FROM route
-							WHERE
-							ROUTE_ID = ' . $_GET['id'];
-						$result = mysqli_query($db, $query) or die(mysqli_error($db));
-						
-?>
-			<script type="text/javascript">
-				alert("Successfully Deleted.");
-				window.location = " route.php";
-			</script>				
+    <?php
+    if (!isset($_GET['do']) || $_GET['do'] != 1) {
+        switch ($_GET['type']) {
+            case 'route':
+                $query = 'DELETE FROM route WHERE ROUTE_ID = ' . $_GET['id'];
+                $result = mysqli_query($db, $query) or die(mysqli_error($db));			
+    ?>
+    <script type="text/javascript">
+        alert("Successfully Deleted.");
+        window.location = " route.php";
+    </script>				
 				
-			<?php
-			//break;
-				}
-			}
-			?>
+    <?php
+    //break;
+    }
+}
+?>
 
 </body>
 </html>

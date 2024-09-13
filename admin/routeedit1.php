23f9a938ec68144f7416336c0490328d26901652
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-13 08:04:56 +0100

    Working on Route Edition

--- admin/routeedit1.php
+++ admin/routeedit1.php
@@ -10,7 +10,7 @@ $str = $_POST['START'];
 $fsh = $_POST['FINISH'];
 					
 $query = 'UPDATE route set FAIR ="'.$fr.'",START ="'.$str.'",FINISH ="'.$fsh.'" WHERE ROUTE_ID="'.$zz.'"';
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
 							
 ?>	
 	<script type="text/javascript">

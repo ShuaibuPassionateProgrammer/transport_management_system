6c67926b5cf72e67657a6e26d1f0e8e7ec059403
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-13 16:23:26 +0100

    Deleting a Bus Schedule

--- admin/sheduledel.php
+++ admin/sheduledel.php
@@ -8,26 +8,26 @@ include('header.php');
 
         
 
-                if (!isset($_GET['do']) || $_GET['do'] != 1) {
-                                    
+    if (!isset($_GET['do']) || $_GET['do'] != 1) {
+                        
+
+    switch ($_GET['type']) {
+        case 'schedule':
+            $query = 'DELETE FROM schedule
+                    WHERE
+                    SCHEDULE_ID = ' . $_GET['id'];
+                $result = mysqli_query($db, $query) or die(mysqli_error($db));
+                
+?>
+    <script type="text/javascript">
+        alert("Successfully Deleted.");
+        window.location = "schedule.php"; 9
+    </script>				
         
-                switch ($_GET['type']) {
-                    case 'schedule':
-                        $query = 'DELETE FROM schedule
-                                WHERE
-                                SCHEDULE_ID = ' . $_GET['id'];
-                            $result = mysqli_query($db, $query) or die(mysqli_error($db));
-                            
+    <?php
+    //break;
+        }
+    }
     ?>
-                <script type="text/javascript">
-                    alert("Successfully Deleted.");
-                    window.location = "schedule.php"; 9
-                </script>				
-                    
-                <?php
-                //break;
-                    }
-                }
-                ?>
 </body>
 </html>

9d5eefcf9f1497057d7a36b884a0d773bf8fb20c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-13 08:06:02 +0100

    Working on Route Edition

--- admin/routeedit1.php
+++ admin/routeedit1.php
@@ -3,18 +3,18 @@ include 'connection.php';
 include 'topnav.php'; 
 ?>
 <body>
-<?php
-$zz = $_POST['id'];
-$fr = $_POST['FAIR'];
-$str = $_POST['START'];
-$fsh = $_POST['FINISH'];
-					
-$query = 'UPDATE route set FAIR ="'.$fr.'",START ="'.$str.'",FINISH ="'.$fsh.'" WHERE ROUTE_ID="'.$zz.'"';
-$result = mysqli_query($db, $query) or die(mysqli_error($db));
-							
-?>	
-	<script type="text/javascript">
+    <?php
+    $zz = $_POST['id'];
+    $fr = $_POST['FAIR'];
+    $str = $_POST['START'];
+    $fsh = $_POST['FINISH'];
+                        
+    $query = 'UPDATE route set FAIR ="'.$fr.'",START ="'.$str.'",FINISH ="'.$fsh.'" WHERE ROUTE_ID="'.$zz.'"';
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+                                
+    ?>	
+    <script type="text/javascript">
         alert("Update Successfull.");
         window.location = "route.php";
-	</script>
+    </script>
 <?php include 'footer.php'; ?>

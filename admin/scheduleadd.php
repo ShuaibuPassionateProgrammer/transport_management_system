b1b0e9143e0a23ad37bcabdaa2a5eba7c44726a7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-13 14:14:31 +0100

    Creating a Bus Schedule

--- admin/scheduleadd.php
+++ admin/scheduleadd.php
@@ -26,7 +26,8 @@ include 'topnav.php';
                     $query = mysqli_query($db, $sql);
                     if(mysqli_num_rows($query) > 0) {
                         while($row = mysqli_fetch_assoc($query)) {
-                        ?><option value="<?php echo $row['BUS_ID'];?>"><?php echo $row['BUS_NAME'];?></option> <?php
+                        ?>
+                        <option value="<?php echo $row['BUS_ID'];?>"><?php echo $row['BUS_NAME'];?></option> <?php
                         }
                     }
                     else {

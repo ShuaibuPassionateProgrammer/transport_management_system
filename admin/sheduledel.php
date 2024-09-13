212915b48fb2ee9e406fe4cde89fa89462846df1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-13 16:24:26 +0100

    Deleting a Bus Schedule

--- admin/sheduledel.php
+++ admin/sheduledel.php
@@ -9,10 +9,8 @@ include('header.php');
 
     switch ($_GET['type']) {
         case 'schedule':
-            $query = 'DELETE FROM schedule
-                    WHERE
-                    SCHEDULE_ID = ' . $_GET['id'];
-                $result = mysqli_query($db, $query) or die(mysqli_error($db));
+            $query = 'DELETE FROM schedule WHERE SCHEDULE_ID = ' . $_GET['id'];
+            $result = mysqli_query($db, $query) or die(mysqli_error($db));
                 
     ?>
     <script type="text/javascript">

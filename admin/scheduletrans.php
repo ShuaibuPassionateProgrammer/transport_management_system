4efc6817cbeef7f75e7cf4af70df4fc87f7a3ed8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-16 16:15:04 +0100

    Edting a Bus Schedule

--- admin/scheduletrans.php
+++ admin/scheduletrans.php
@@ -2,23 +2,23 @@
 <?php include'connection.php'; ?>
 
 <div class="col-lg-12">
-                <?php
-						$arvl= $_POST['ARRIVAL'];
-					    $dpt= $_POST['DEPARTURE'];
-						$bid= $_POST['BUS_ID'];
-						
-				
-					switch($_GET['action']){
-						case 'add':			
-								$query = "INSERT INTO schedule
-								(ARRIVAL, DEPARTURE, BUS_ID)
-								VALUES ('".$arvl."','".$dpt."','".$bid."')";
-								mysqli_query($db,$query)or die (mysqli_error($db));
-							
-						break;
-									
-						}
-				?>
+    <?php
+            $arvl= $_POST['ARRIVAL'];
+            $dpt= $_POST['DEPARTURE'];
+            $bid= $_POST['BUS_ID'];
+            
+    
+        switch($_GET['action']){
+            case 'add':			
+                    $query = "INSERT INTO schedule
+                    (ARRIVAL, DEPARTURE, BUS_ID)
+                    VALUES ('".$arvl."','".$dpt."','".$bid."')";
+                    mysqli_query($db,$query)or die (mysqli_error($db));
+                
+            break;
+                        
+            }
+    ?>
     	<script type="text/javascript">
 			alert("Successfully added.");
 			window.location = "schedule.php";

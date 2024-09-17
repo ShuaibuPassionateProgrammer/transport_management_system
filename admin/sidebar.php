742b784225ff0345339e92ff728e369fc2fc63e1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-17 10:23:37 +0100

    Creating a sidebar navigation

---
+++ admin/sidebar.php
@@ -0,0 +1,54 @@
+
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav"> <!-- style="background-image: linear-gradient(pink,#2144);" -->
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Bus.php">
+            <i class="fas fa-fw fa-bus"></i>
+            <span>Vehicle</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="driver.php">
+            <i class="fas fa-fw fa-users    "></i>
+            <span>Driver</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="passenger.php">
+            <i class="fas fa-fw fa-users    "></i>
+            <span>Passenger</span></a>
+        </li>
+        
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="route.php">
+            <i class="fas fa-fw fa-route   "></i>
+            <span>Route</span></a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="schedule.php">
+            <i class="fas fa-fw fa-clock  "></i>
+            <span>Schedule</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="stop.php">
+            <i class="fas fa fa-map-marker"></i>
+            <span>Stop</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="feedback.php">
+            <i class="fas fa fas fa-bell"></i>
+            <span>Feed Back</span></a>
+          </li>
+      </ul>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">

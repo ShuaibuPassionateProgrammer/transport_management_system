f5fee5e3d01e62f3c114fcc20d53ede9d5e27b5e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-19 16:39:40 +0100

    Deleting a Bus stop

--- admin/stopdel.php
+++ admin/stopdel.php
@@ -4,31 +4,24 @@ include('header.php');
 ?>  
 
 <body>
-<?php
+    <?php
+    if (!isset($_GET['do']) || $_GET['do'] != 1) {
 
-	
-
-			if (!isset($_GET['do']) || $_GET['do'] != 1) {
-								
-	
-			switch ($_GET['type']) {
-				case 'stop':
-					$query = 'DELETE FROM stop
-							WHERE
-							LOCATION_ID = ' . $_GET['id'];
-						$result = mysqli_query($db, $query) or die(mysqli_error($db));
-						
-?>
-			<script type="text/javascript">
-				alert("Successfully Deleted.");
-				window.location = "stop.php";
-			</script>				
-				
-			<?php
-			//break;
-				}
-			}
-			?>
+    switch ($_GET['type']) {
+        case 'stop':
+            $query = 'DELETE FROM stop WHERE LOCATION_ID = ' . $_GET['id'];
+            $result = mysqli_query($db, $query) or die(mysqli_error($db));    
+    ?>
+    <script type="text/javascript">
+        alert("Successfully Deleted.");
+        window.location = "stop.php";
+    </script>				
+        
+    <?php
+    //break;
+        }
+    }
+    ?>
 
 </body>
 </html>

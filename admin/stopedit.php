1136bb223a840845cc0703dfb566425c0692aa0b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-21 07:43:55 +0100

    Editing a Bus stop

--- admin/stopedit.php
+++ admin/stopedit.php
@@ -22,26 +22,25 @@ include 'topnav.php'; ?>
     <div class="col-lg-12" style="background-image: linear-gradient(black);">
             <h1>Edit Records</h1>
                 <div class="col-lg-6"> 
+                    <form role="form" method="post" action="stopedit1.php">
+                        
+                        <div class="form-group">
+                            <input type="hidden" name="id" value="<?php echo $zz; ?>" />
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Location Name" name="LOCATION_NAME" value="<?php echo $i; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Route ID" name="ROUTE_ID" value="<?php echo $a; ?>">
+                        </div>
 
-                <form role="form" method="post" action="stopedit1.php">
-                    
-                    <div class="form-group">
-                        <input type="hidden" name="id" value="<?php echo $zz; ?>" />
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Location Name" name="LOCATION_NAME" value="<?php echo $i; ?>">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Route ID" name="ROUTE_ID" value="<?php echo $a; ?>">
-                    </div>
-
-                    <button type="submit" class="btn btn-default">Update Record</button>
-                    <br></br>
-                </form>  
+                        <button type="submit" class="btn btn-default">Update Record</button>
+                        <br></br>
+                    </form>  
+                </div>
             </div>
-        </div>
         
+        </div>
+        <!-- /.container-fluid -->
     </div>
-    <!-- /.container-fluid -->
-</div>
 <?php include 'footer.php'; ?>

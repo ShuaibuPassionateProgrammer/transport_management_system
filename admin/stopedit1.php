00e1b8597bb43ab5149d4801660bc75f309a2ad8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-21 07:45:33 +0100

    Editing a Bus stop

--- admin/stopedit1.php
+++ admin/stopedit1.php
@@ -1,18 +1,17 @@
-<?php include 'connection.php';
-include 'topnav.php'; ?>
+<?php 
+include 'connection.php';
+include 'topnav.php';
+?>
 <body>
-<?php
-			$zz = $_POST['id'];
-			$ln = $_POST['LOCATION_NAME'];
-		    $rid = $_POST['ROUTE_ID'];
-			
-		
-	 			$query = 'UPDATE stop set LOCATION_NAME ="'.$ln.'",
-					ROUTE_ID ="'.$rid.'"  WHERE
-					LOCATION_ID ="'.$zz.'"';
-					$result = mysqli_query($db, $query) or die(mysqli_error($db));
-							
-?>	
+    <?php
+    $zz = $_POST['id'];
+    $ln = $_POST['LOCATION_NAME'];
+    $rid = $_POST['ROUTE_ID'];
+    
+    $query = 'UPDATE stop set LOCATION_NAME ="'.$ln.'", ROUTE_ID ="'.$rid.'"  WHERE LOCATION_ID ="'.$zz.'"';
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
+                                
+    ?>	
 	<script type="text/javascript">
 			alert("Update Successfull.");
 			window.location = "stop.php";

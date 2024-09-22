4e27fb33a6bdf2cea4bb5e4f5cc06d7741a0adb9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-22 05:57:30 +0100

    Searching a Bus stop

--- admin/stopsearch.php
+++ admin/stopsearch.php
@@ -1,55 +1,52 @@
-
-<?php include 'connection.php';
+<?php 
+include 'connection.php';
 include 'topnav.php'
 ?>
 
 <div class="contanier">
-<div class="card card-register mx-auto mt-5">
-<div class="card-header"> <h2>Check Record</h2> </div>
-<div class="card-body">
-
-<?php
-$query = 'SELECT * FROM stop
-              WHERE
-              LOCATION_ID ='.$_GET['id'];
-            $result = mysqli_query($db, $query) or die(mysqli_error($db));
-              while($row = mysqli_fetch_array($result))
-              {   
-                $zz= $row['LOCATION_ID'];
-                $i= $row['LOCATION_NAME'];
-                $a=$row['ROUTE_ID'];
-              }
-              
-              $id = $_GET['id'];
-         
-?>
-
-
-                        <form role="form" method="post" action="stop.php">
-                            
-                            <div class="form-group">
-                                <input type="hidden" name="id" value="<?php echo $zz; ?>" />
-                            </div>
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Location Name" name="LOCATION_NAME" value="<?php echo $i; ?>">
-                            </div>
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Route ID" name="ROUTE_ID" value="<?php echo $a; ?>">
-                            </div>
-
-                            <button type="submit" class="btn btn-default">Return to main menu</button>
-                         
-
-
-                      </form>  
-                    </div>
-                </div>
+    <div class="card card-register mx-auto mt-5">
+        <div class="card-header"> <h2>Check Record</h2> </div>
+        <div class="card-body">
+            <?php
+            $query = 'SELECT * FROM stop
+                        WHERE
+                        LOCATION_ID ='.$_GET['id'];
+                        $result = mysqli_query($db, $query) or die(mysqli_error($db));
+                        while($row = mysqli_fetch_array($result))
+                        {   
+                            $zz= $row['LOCATION_ID'];
+                            $i= $row['LOCATION_NAME'];
+                            $a=$row['ROUTE_ID'];
+                        }
+                        
+                        $id = $_GET['id'];
+                    
+            ?>
+            <form role="form" method="post" action="stop.php">
                 
-            </div>
-            <!-- /.container-fluid -->
+                <div class="form-group">
+                    <input type="hidden" name="id" value="<?php echo $zz; ?>" />
+                </div>
+                <div class="form-group">
+                    <input class="form-control" placeholder="Location Name" name="LOCATION_NAME" value="<?php echo $i; ?>">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" placeholder="Route ID" name="ROUTE_ID" value="<?php echo $a; ?>">
+                </div>
 
+                <button type="submit" class="btn btn-default">Return to main menu</button>
+                
+
+
+            </form>  
         </div>
-        <!-- /#page-wrapper -->
+    </div>
+    
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
 
     </div>
     <!-- /#wrapper -->

b8088e149d637a553c97413069941447a7623032
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-23 10:04:49 +0100

    Managing a successful state

--- admin/success.php
+++ admin/success.php
@@ -14,38 +14,38 @@ $result = $db->query($sql);
 //get the number of result based n the sql statement
 //check the number of result, if equal to one
 //If theres a result
-    if($result->num_rows > 0){
-        //store the result to a array and passed to variable found_user
-        $found_user = mysqli_fetch_array($result);
+if($result->num_rows > 0){
+    //store the result to a array and passed to variable found_user
+    $found_user = mysqli_fetch_array($result);
 
-        //fill the result to session variable
-        $_SESSION['MEMBER_ID'] = $found_user['id'];
-        $_SESSION['fname'] = $found_user['fname'];
-        $_SESSION['lname'] = $found_user['lname'];
-    $_SESSION['position'] = $found_user['status'];
-        if ($_SESSION['position']=='ADMIN') {
-        # code...
-    ?> <script type="text/javascript">
-            //then it will be redirected to index.php
-            window.location = "index.php"
-    </script>
-    <?php
-        }else{
-        ?> <script type="text/javascript">
-            //then it will be redirected to index.php
-            window.location = "homepage.php"
-    </script>
-    <?php
-        }
-
+    //fill the result to session variable
+    $_SESSION['MEMBER_ID'] = $found_user['id'];
+    $_SESSION['fname'] = $found_user['fname'];
+    $_SESSION['lname'] = $found_user['lname'];
+$_SESSION['position'] = $found_user['status'];
+    if ($_SESSION['position']=='ADMIN') {
+    # code...
+?> <script type="text/javascript">
+        //then it will be redirected to index.php
+        window.location = "index.php"
+</script>
+<?php
     }else{
-//if theres no result
     ?> <script type="text/javascript">
-        alert("Invalid Username or Password!");
-        window.location = "login.php";
-        </script>
-    <?php
+        //then it will be redirected to index.php
+        window.location = "homepage.php"
+</script>
+<?php
     }
+
+}else{
+//if theres no result
+?> <script type="text/javascript">
+    alert("Invalid Username or Password!");
+    window.location = "login.php";
+    </script>
+<?php
+}
     
 $db->close();
 ?>

f01cd03a70860111247047957128bd9be7155d66
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-09-26 10:13:32 +0100

    Working on User bus

--- admin/userbus.php
+++ admin/userbus.php
@@ -88,38 +88,35 @@ confirm_logged_in();
       </ul>
 
       <div id="content-wrapper">
-
         <div class="container-fluid">
-
            <div class="col-lg-12">
-
             <div>
             <i class="fas fa-table"></i>
-
-          <br> </br>
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>Bus Name</th>
-                                        <th>Bus Type</th>
-                                        <th>Driver ID</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                 <?php                  
-                $query = 'SELECT * FROM bus';
-                    $result = mysqli_query($db, $query) or die (mysqli_error($db));
-                  
+            <br> </br>
+            
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Bus Name</th>
+                            <th>Bus Type</th>
+                            <th>Driver ID</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php                  
+                        $query = 'SELECT * FROM bus';
+                        $result = mysqli_query($db, $query) or die (mysqli_error($db));
+        
                         while ($row = mysqli_fetch_assoc($result)) {
-                                             
+                                                
                             echo '<tr>';
                             echo '<td>'. $row['BUS_NAME'].'</td>';
                             echo '<td>'. $row['BUS_TYPE'].'</td>';
                             echo '<td>'. $row['DRIVER_ID'].'</td>';
                             echo '</tr> ';
-                }
-            ?> 
+                        }
+                    ?> 
                                     
                                 </tbody>
                             </table>

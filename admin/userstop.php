c20f37820aadce3ec051ffd46b0da2480311b9dd
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-10-02 12:36:24 +0100

    Working on User bus stop

--- admin/userstop.php
+++ admin/userstop.php
@@ -91,37 +91,35 @@ confirm_logged_in();
       <div id="content-wrapper">
         <div class="container-fluid">
            <div class="col-lg-12">
-
             <div>
             <i class="fas fa-table"></i>
-
-            
             </div>    
 
           <br> </br>
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>Location Name</th>
-                                        <th>Route ID</th>
-                                       
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                 <?php                  
-                $query = 'SELECT * FROM stop';
-                    $result = mysqli_query($db, $query) or die (mysqli_error($db));
-                  
-                        while ($row = mysqli_fetch_assoc($result)) {
-                                             
-                            echo '<tr>';
-                            echo '<td>'. $row['LOCATION_NAME'].'</td>';
-                            echo '<td>'. $row['ROUTE_ID'].'</td>';
+          
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Location Name</th>
+                            <th>Route ID</th>
                             
-                            echo '</tr> ';
-                }
-            ?> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php                  
+    $query = 'SELECT * FROM stop';
+        $result = mysqli_query($db, $query) or die (mysqli_error($db));
+        
+            while ($row = mysqli_fetch_assoc($result)) {
+                                    
+                echo '<tr>';
+                echo '<td>'. $row['LOCATION_NAME'].'</td>';
+                echo '<td>'. $row['ROUTE_ID'].'</td>';
+                
+                echo '</tr> ';
+    }
+?> 
                                     
                                 </tbody>
                             </table>

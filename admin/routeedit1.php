62a7664804668bff2692e24a0f5c0e8d1df91c4f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-25 19:52:21 +0100

    Route edition improved

--- admin/routeedit1.php
+++ admin/routeedit1.php
@@ -4,17 +4,24 @@ include 'topnav.php';
 ?>
 <body>
     <?php
-    $zz = $_POST['id'];
-    $fr = $_POST['FAIR'];
-    $str = $_POST['START'];
-    $fsh = $_POST['FINISH'];
-                        
-    $query = 'UPDATE route set FAIR ="'.$fr.'",START ="'.$str.'",FINISH ="'.$fsh.'" WHERE ROUTE_ID="'.$zz.'"';
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
-                                
-    ?>	
-    <script type="text/javascript">
-        alert("Update Successfull.");
-        window.location = "route.php";
-    </script>
+    $zz = trim($_POST['id']);
+    $fr = trim($_POST['FAIR']);
+    $str = trim($_POST['START']);
+    $fsh = trim($_POST['FINISH']);
+
+    if (empty($zz) || empty($fr) || empty($str) || empty($fsh)) {
+        echo '<script>alert("All fields are required.");window.history.back();</script>';
+    } else {
+        $query = 'UPDATE route SET FAIR = ?, START = ?, FINISH = ? WHERE ROUTE_ID = ?';
+        $stmt = mysqli_prepare($db, $query);
+        mysqli_stmt_bind_param($stmt, "sssi", $fr, $str, $fsh, $zz);
+
+        if (mysqli_stmt_execute($stmt)) {
+            echo '<script>alert("Update Successful.");window.location = "route.php";</script>';
+        } else {
+            error_log("Error updating route: " . mysqli_error($db));
+            echo '<script>alert("An error occurred. Please try again.");window.history.back();</script>';
+        }
+        mysqli_stmt_close($stmt);
+    }
 <?php include 'footer.php'; ?>

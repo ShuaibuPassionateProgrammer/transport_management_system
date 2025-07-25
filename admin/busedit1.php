981bc1260f502bdc7cb75a12876ff667ca81e44f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-25 19:38:42 +0100

    Bus edit

--- admin/busedit1.php
+++ admin/busedit1.php
@@ -5,17 +5,24 @@ include 'topnav.php';
 
 <body>
     <?php
-    $zz = $_POST['id'];
-    $bname = $_POST['BUS_NAME'];
-    $btype = $_POST['BUS_TYPE'];
-    $did= $_POST['DRIVER_ID'];
-                
-    $query = 'UPDATE bus set BUS_NAME ="'.$bname.'", BUS_TYPE ="'.$btype.'", DRIVER_ID="'.$did.'" WHERE BUS_ID ="'.$zz.'"';
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
-                                
-    ?>	
-    <script type="text/javascript">
-        alert("Update Successfull.");
-        window.location = "bus.php";
-    </script>
+    $zz = trim($_POST['id']);
+    $bname = trim($_POST['BUS_NAME']);
+    $btype = trim($_POST['BUS_TYPE']);
+    $did = trim($_POST['DRIVER_ID']);
+
+    if (empty($zz) || empty($bname) || empty($btype) || empty($did)) {
+        echo '<script>alert("All fields are required.");window.history.back();</script>';
+    } else {
+        $query = 'UPDATE bus SET BUS_NAME = ?, BUS_TYPE = ?, DRIVER_ID = ? WHERE BUS_ID = ?';
+        $stmt = mysqli_prepare($db, $query);
+        mysqli_stmt_bind_param($stmt, "ssii", $bname, $btype, $did, $zz);
+
+        if (mysqli_stmt_execute($stmt)) {
+            echo '<script>alert("Update Successful.");window.location = "bus.php";</script>';
+        } else {
+            error_log("Error updating bus: " . mysqli_error($db));
+            echo '<script>alert("An error occurred. Please try again.");window.history.back();</script>';
+        }
+        mysqli_stmt_close($stmt);
+    }
  <?php include 'footer.php'; ?>

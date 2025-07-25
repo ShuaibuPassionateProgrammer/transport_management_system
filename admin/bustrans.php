c2a52bdead2085a13ee0e5588e88de72a1e70f3e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-25 19:38:29 +0100

    Bus transport improved

--- admin/bustrans.php
+++ admin/bustrans.php
@@ -4,23 +4,28 @@
 <div class="col-lg-12">
     <?php
     // $bid= $_POST['id'];
-    $bname= $_POST['BUS_NAME'];
-    $btype= $_POST['BUS_TYPE'];
-    $did= $_POST['DRIVER_ID'];
+    $bname = trim($_POST['BUS_NAME']);
+    $btype = trim($_POST['BUS_TYPE']);
+    $did = trim($_POST['DRIVER_ID']);
 
-    switch($_GET['action']){
-        case 'add':			
-                $query = "INSERT INTO bus
-                (BUS_NAME, BUS_TYPE, DRIVER_ID)
-                VALUES ('".$bname."','".$btype."','".$did."')";
-                mysqli_query($db,$query)or die (mysqli_error($db));
-            
-        break;
-                    
+    if (isset($_GET['action']) && $_GET['action'] == 'add') {
+        if (empty($bname) || empty($btype) || empty($did)) {
+            echo '<script>alert("All fields are required.");window.history.back();</script>';
+        } else {
+            $query = "INSERT INTO bus (BUS_NAME, BUS_TYPE, DRIVER_ID) VALUES (?, ?, ?)";
+            $stmt = mysqli_prepare($db, $query);
+            mysqli_stmt_bind_param($stmt, "ssi", $bname, $btype, $did);
+
+            if (mysqli_stmt_execute($stmt)) {
+                echo '<script>alert("Successfully added.");window.location = "bus.php";</script>';
+            } else {
+                error_log("Error adding bus: " . mysqli_error($db));
+                echo '<script>alert("An error occurred. Please try again.");window.history.back();</script>';
+            }
+            mysqli_stmt_close($stmt);
+        }
+    } else {
+        header("Location: bus.php");
+        exit();
     }
-    ?>
-    <script type="text/javascript">
-        alert("Successfully added.");
-        window.location = "bus.php";
-    </script>
 </div>

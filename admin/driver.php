4c73da741bb16eb865aa378bc2302f549c106be2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-25 19:35:51 +0100

    Driver improved

--- admin/driver.php
+++ admin/driver.php
@@ -29,10 +29,10 @@ include 'topnav.php';
                 while ($row = mysqli_fetch_assoc($result)) {
                                         
                     echo '<tr>';
-                    echo '<td>'. $row['DRIVER_NAME'].'</td>';
-                    echo '<td>'. $row['DRIVER_EMAIL'].'</td>';
-                    echo '<td>'. $row['DRIVER_PHONE'].'</td>';
-                    echo '<td>'. $row['EMPLOY_DATE'].'</td>';
+                    echo '<td>'. htmlspecialchars($row['DRIVER_NAME']).'</td>';
+                    echo '<td>'. htmlspecialchars($row['DRIVER_EMAIL']).'</td>';
+                    echo '<td>'. htmlspecialchars($row['DRIVER_PHONE']).'</td>';
+                    echo '<td>'. htmlspecialchars($row['EMPLOY_DATE']).'</td>';
                     echo '<td> <a type="button" class="btn btn-xs btn-primary" href="driversearch.php?action=edit & id='.$row['DRIVER_ID'] . '" > SEARCH </a> ';
                     echo ' <a  type="button" class="btn btn-xs btn-warning" href="driveredit.php?action=edit & id='.$row['DRIVER_ID'] . '"> EDIT </a> ';
                     echo ' <a  type="button" class="btn btn-xs btn-danger" href="driverdel.php?type=driver&delete & id='.$row['DRIVER_ID'] . '" onclick="return confirm(\'Are you sure, you want to Delete a Driver\');">DELETE </a> </td>';

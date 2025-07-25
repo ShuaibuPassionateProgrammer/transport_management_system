40e27d627e1340c28f5f8cb12ea347e48d633740
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-25 19:51:50 +0100

    Driver Edit improved

--- admin/driveredit1.php
+++ admin/driveredit1.php
@@ -2,17 +2,25 @@
 include 'topnav.php'; ?>
 <body>
 <?php
-$d_id = $_POST['id'];
-$dname = $_POST['DRIVER_NAME'];
-$demail = $_POST['DRIVER_EMAIL'];
-$dphone = $_POST['DRIVER_PHONE'];
-$edate = $_POST['EMPLOY_DATE'];
-					
-$query = 'UPDATE driver set DRIVER_NAME ="'.$dname.'", DRIVER_EMAIL ="'.$demail.'", DRIVER_PHONE ="'.$dphone.'", EMPLOY_DATE ="'.$edate.'" WHERE DRIVER_ID ="'.$d_id.'"';
-$result = mysqli_query($db, $query) or die(mysqli_error($db));
-?>	
-<script type="text/javascript">
-	alert("Update Successfull.");
-	window.location = "driver.php";
-</script>
+$d_id = trim($_POST['id']);
+$dname = trim($_POST['DRIVER_NAME']);
+$demail = trim($_POST['DRIVER_EMAIL']);
+$dphone = trim($_POST['DRIVER_PHONE']);
+$edate = trim($_POST['EMPLOY_DATE']);
+
+if (empty($d_id) || empty($dname) || empty($demail) || empty($dphone) || empty($edate)) {
+    echo '<script>alert("All fields are required.");window.history.back();</script>';
+} else {
+    $query = 'UPDATE driver SET DRIVER_NAME = ?, DRIVER_EMAIL = ?, DRIVER_PHONE = ?, EMPLOY_DATE = ? WHERE DRIVER_ID = ?';
+    $stmt = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($stmt, "ssssi", $dname, $demail, $dphone, $edate, $d_id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo '<script>alert("Update Successful.");window.location = "driver.php";</script>';
+    } else {
+        error_log("Error updating driver: " . mysqli_error($db));
+        echo '<script>alert("An error occurred. Please try again.");window.history.back();</script>';
+    }
+    mysqli_stmt_close($stmt);
+}
  <?php include 'footer.php'; ?>
